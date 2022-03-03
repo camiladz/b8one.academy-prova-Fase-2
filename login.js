484a51b0df66698b47fdfd295bd966f4d6e5c408
CamilaDZ <camifstack@gmail.com>
2022-03-03 14:14:53 -0300

    alteração css tela de login validação de email e visualização da senha

---
+++ login.js
@@ -0,0 +1,33 @@
+/* -- validar email -- */
+function validateEmail() {
+    var email = document.querySelector('.login-form__input');
+    var erro = document.querySelector('.erro');
+
+    if(!email.checkValidity()){
+        erro.innerHTML = "Este e-mail não existe, você tem outro?";
+    }
+
+}
+
+function changeMessage(){
+    var erro = document.querySelector('.erro');
+    if(erro.innerHTML == "Este e-mail não existe, você tem outro?"){
+        erro.innerHTML = "";
+    }
+}
+
+/* -- mostrar senha -- */
+let mostrarSenha = document.querySelector('.login-form__input--pass');
+console.log(mostrarSenha)
+let icon = document.querySelector('.visible__button');
+
+  icon.addEventListener("click", function(){
+       if(mostrarSenha.type == 'password'){
+           mostrarSenha.type ='text';
+           icon.src = 'assets/images/visible.svg';
+       } else {
+           mostrarSenha.type = 'password';
+           icon.src = 'assets/images/visible-off.svg';
+       }
+   })
+
